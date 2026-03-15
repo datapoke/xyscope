7edf053059e9faf9c7bdc74bcc4f462d440191c6
Christopher Reaume <christopher.reaume@automattic.com>
2026-03-14 20:23:54 -0700

    fix: normalize build

--- build_windows.sh
+++ build_windows.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-#
-# Cross-compile XYScope for Windows using Docker + MinGW-w64
-# Produces release/xyscope.exe with required DLLs
-#
-
-set -e
-
-IMAGE=xyscope-win
-
-echo "Building Docker image..."
-docker build -f Dockerfile.windows -t $IMAGE .
-
-echo "Cross-compiling for Windows..."
-docker run --rm -v "$(pwd)":/src $IMAGE
-
-echo ""
-echo "Done! Windows build:"
-ls -lh release/xyscope.exe release/*.dll
+#!/bin/sh
+docker build -f Dockerfile.windows -t xyscope-win .
+docker run -it --rm -v $(pwd):/usr/src/xyscope.dist xyscope-win
